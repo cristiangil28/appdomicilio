f427b26030b72a29a723f19d2dad70d95f3440d8
cristian gil <cristianingeniero2018@hotmail.com>
2020-02-07 14:02:39 -0500

    app de comicilio con .net

---
+++ Domicilio/Controllers/PedidosController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace Domicilio.Controllers
+{
+    public class PedidosController : Controller
+    {
+        private pedidosEntities db = new pedidosEntities();
+
+        // GET: Pedidos
+        public async Task<ActionResult> Index()
+        {
+            var pedidos = db.Pedidos.Include(p => p.Clientes).Include(p => p.Productos);
+            return View(await pedidos.ToListAsync());
+        }
+
+        // GET: Pedidos/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedidos pedidos = await db.Pedidos.FindAsync(id);
+            if (pedidos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedidos);
+        }
+
+        // GET: Pedidos/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_cliente = new SelectList(db.Clientes, "id", "cedula");
+            ViewBag.id_producto = new SelectList(db.Productos, "id_producto", "plu");
+            return View();
+        }
+
+        // POST: Pedidos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id_pedido,nombre_recibe,observacion_pedido,observacion_producto,total_productos,valor_total,id_producto,id_cliente")] Pedidos pedidos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pedidos.Add(pedidos);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_cliente = new SelectList(db.Clientes, "id", "cedula", pedidos.id_cliente);
+            ViewBag.id_producto = new SelectList(db.Productos, "id_producto", "plu", pedidos.id_producto);
+            return View(pedidos);
+        }
+
+        // GET: Pedidos/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedidos pedidos = await db.Pedidos.FindAsync(id);
+            if (pedidos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_cliente = new SelectList(db.Clientes, "id", "cedula", pedidos.id_cliente);
+            ViewBag.id_producto = new SelectList(db.Productos, "id_producto", "plu", pedidos.id_producto);
+            return View(pedidos);
+        }
+
+        // POST: Pedidos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id_pedido,nombre_recibe,observacion_pedido,observacion_producto,total_productos,valor_total,id_producto,id_cliente")] Pedidos pedidos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pedidos).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_cliente = new SelectList(db.Clientes, "id", "cedula", pedidos.id_cliente);
+            ViewBag.id_producto = new SelectList(db.Productos, "id_producto", "plu", pedidos.id_producto);
+            return View(pedidos);
+        }
+
+        // GET: Pedidos/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedidos pedidos = await db.Pedidos.FindAsync(id);
+            if (pedidos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedidos);
+        }
+
+        // POST: Pedidos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Pedidos pedidos = await db.Pedidos.FindAsync(id);
+            db.Pedidos.Remove(pedidos);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Domicilio/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante lo siguiente
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Domicilio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Domicilio")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si configura ComVisible como falso, los tipos de este ensamblado no se hacen visibles
+// para componentes COM.  Si necesita acceder a un tipo de este ensamblado desde
+// COM, establezca el atributo ComVisible en True en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como ID de typelib si este proyecto se expone a COM
+[assembly: Guid("280c2297-2dd1-4040-82ac-e2bec4ea1ba1")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o puede predeterminar los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
